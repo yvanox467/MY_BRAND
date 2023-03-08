9f1909cd351eac210ecd8f98401fefdbb75c3e6d
yvanox467 <106058620+yvanox467@users.noreply.github.com>
2023-03-08 14:40:33 +0000

    Application on Json

--- js/comment.js
+++ js/comment.js
@@ -1,11 +1,15 @@
-const form = document.querySelector('.low');
+/*const form = document.getElementById('form')
 const createPost = async (e) => {
   e.preventDefault();
+  const name = document.getElementById('001').value;
+  const email = document.getElementById('002').value;
+  const contact = document.getElementById('003').value;
+  const comment = document.getElementById('004').value;
   const doc = {
-    name: form.name.value,
-    email:form.email.value,
-    contact:form.contact.value,
-    comments:form.comment.value,
+    name: name,
+    email: email,
+    contact: contact,
+    comment: comment,
   };
   await fetch('http://localhost:3000/comments', {
     method: 'POST',
@@ -13,30 +17,21 @@ const createPost = async (e) => {
     headers: { 'Content-Type': 'application/json' },
   });
 };
-form.addEventListener('submit', createPost);
-const container = document.querySelector('container');
-const renderPosts = async () => {
-  let uri = 'http://localhost:3000/comments';
-  const res = await fetch(uri);
-  const wrapper = await res.json();
-  let template = ``;
-  wrapper.forEach((posts) => {
-    template += `
-      <div class="rap">
-      <label for="">Names:</label><br>
-      <label for="">Email:</label><br>
-      <label for="">Contact:</label><br>
-      <label for="">Comment:</label>
-      <p>lorem ipsum</p>
-      </div>
-    `;
+form.addEventListener('submit', createPost);*/
+const form = document.getElementById('form')
+const createComment = async (e) => {
+  e.preventDefault();
+  const doc = {
+      name: form.name.value,
+      email: form.email.value,
+      contact: form.contact.value,
+      body: form.comment.value,
+  }
+  await fetch('http://localhost:3000/posts', {
+      method: 'POST',
+      body: JSON.stringify(doc),
+      headers: { 'Content-Type': 'application/json' }
   });
-  container.innerHTML = template;
-};
-window.addEventListener('DOMContentLoaded', () => renderPosts());
-
-
-
-
-
-
+  window.location.replace('/index.html');
+}
+form.addEventListener('submit', createComment);
